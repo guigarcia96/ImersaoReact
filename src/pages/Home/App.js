172eca7ac9183f9e47fd830eb02efee5fb5cf995
Guilherme Moraes <gmoraes1@outlook.com>
2020-08-03 14:40:46 -0300

    Removendo comentarios da Home

--- src/pages/Home/App.js
+++ src/pages/Home/App.js
@@ -49,50 +49,6 @@ function Home() {
         );
       })}
 
-      {/* <BannerMain
-        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
-        videoDescription="Lorem Ipsum bla bla bla"
-        url={dadosIniciais.categorias[0].videos[0].url}
-      />
-
-      <Carousel
-
-        ignoreFirstVideo
-
-        category={dadosIniciais.categorias[0]}
-
-      />
-
-      <Carousel
-
-        category={dadosIniciais.categorias[1]}
-
-      />
-
-      <Carousel
-
-        category={dadosIniciais.categorias[2]}
-
-      />
-
-      <Carousel
-
-        category={dadosIniciais.categorias[3]}
-
-      />
-
-      <Carousel
-
-        category={dadosIniciais.categorias[4]}
-
-      />
-
-      <Carousel
-
-        category={dadosIniciais.categorias[5]}
-
-      /> */}
-
     </PageDefault>
   );
 }
